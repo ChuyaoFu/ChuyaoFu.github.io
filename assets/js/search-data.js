64b4272e33b689f200d91e6139cd4616d8a7499f
ChuyaoFu <168201991+ChuyaoFu@users.noreply.github.com>
2026-09-04 16:40:03 +0000

    Deploying to gh-pages from @ ChuyaoFu/ChuyaoFu.github.io@b35f374b235c77228b1326935a61ea4f8cb89955 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "Publications and ongoing research manuscripts.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-industry-experience",
-          title: "industry experience",
+          title: "Industry Experience",
           description: "Industry research experience in embodied AI, VLA systems, and world models.",
           section: "Navigation",
           handler: () => {
